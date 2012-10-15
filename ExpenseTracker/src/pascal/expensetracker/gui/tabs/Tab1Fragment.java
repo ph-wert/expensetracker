62175c36e7ff7357a601a3395a9f2998a560e498
Pascal <pascal.hagedorn@gmail.com>
2012-10-15 23:22:24 +0200

    addexpense

--- ExpenseTracker/src/pascal/expensetracker/gui/tabs/Tab1Fragment.java
+++ ExpenseTracker/src/pascal/expensetracker/gui/tabs/Tab1Fragment.java
@@ -1,32 +1,26 @@
 package pascal.expensetracker.gui.tabs;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
 import pascal.expensetracker.R;
 import pascal.expensetracker.asynctasks.GetBaseAsyncTask.OnRequestFinishedListener;
 import pascal.expensetracker.asynctasks.GetBaseAsyncTask;
 import pascal.expensetracker.asynctasks.GetJoinedShopsAsyncTask;
 import pascal.expensetracker.asynctasks.GetPersonAsyncTask;
+import pascal.expensetracker.asynctasks.HttpPostAsyncTask;
 import pascal.expensetracker.objects.Expense;
 import pascal.expensetracker.objects.JoinedShops;
 import pascal.expensetracker.objects.Persons;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.GetChars;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -34,18 +28,19 @@ import android.widget.Toast;
  * 
  */
 public class Tab1Fragment extends Fragment {
-	
-	private Persons selectedPerson;
-	private JoinedShops selectedShop;
-	
+
+	private Persons selectedPerson = null;
+	private JoinedShops selectedShop = null;
+
 	private void onButtonClicked() {
 		if (selectedPerson == null || selectedShop == null) {
-			Toast.makeText(getActivity(), "First select your items!", Toast.LENGTH_LONG).show();
+			Toast.makeText(getActivity(), "First select your items!",
+					Toast.LENGTH_LONG).show();
 			return;
 		}
-		
-		Expense currentExpense = new Expense(selectedPerson, selectedShop);
-		
+
+		Expense currentExpense = new Expense(selectedPerson, selectedShop, "2099-9-9", "77.77");
+
 		// todo rest request so save currentexpense...
 	}
 
@@ -55,6 +50,7 @@ public class Tab1Fragment extends Fragment {
 	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater,
 	 *      android.view.ViewGroup, android.os.Bundle)
 	 */
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		if (container == null) {
@@ -79,13 +75,14 @@ public class Tab1Fragment extends Fragment {
 
 						spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-							public void onItemSelected(
-									AdapterView<?> spinnerList, View view,
-									int position, long id) {
+							public void onItemSelected(AdapterView<?> spinnerList, View view, int position, long id) {
 								JoinedShops shop = (JoinedShops) spinnerList
 										.getItemAtPosition(position);
 								// save this shop to a local variable... to use
 								// it later for the rest request
+								Toast.makeText(getActivity(), shop.toString(),
+										Toast.LENGTH_LONG).show();
+								selectedShop = shop;
 							}
 
 							public void onNothingSelected(AdapterView<?> arg0) {
@@ -102,62 +99,65 @@ public class Tab1Fragment extends Fragment {
 					}
 				});
 		task1.execute(new String[] { getString(R.string.host_url) });
-		
-		GetBaseAsyncTask<Persons> task2 = new GetPersonAsyncTask(new OnRequestFinishedListener<Persons>() {
 
-			public void onFinished(ArrayList<Persons> result) {
-				Spinner spinner = (Spinner) getView().findViewById(
-						R.id.spinner_persons);
+		GetBaseAsyncTask<Persons> task2 = new GetPersonAsyncTask(
+				new OnRequestFinishedListener<Persons>() {
+					public void onFinished(ArrayList<Persons> result) {
+						Spinner spinner = (Spinner) getView().findViewById(
+								R.id.spinner_persons);
 
-				spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+						spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-					public void onItemSelected(
-							AdapterView<?> spinnerList, View view,
-							int position, long id) {
-						Persons person = (Persons) spinnerList
-								.getItemAtPosition(position);
-						// save this shop to a local variable... to use
-						// it later for the rest request
-						Toast.makeText(getActivity(), person.toString(), Toast.LENGTH_LONG).show();
-					}
+							public void onItemSelected(
+									AdapterView<?> spinnerList, View view,
+									int position, long id) {
+								Persons person = (Persons) spinnerList
+										.getItemAtPosition(position);
+								// save this shop to a local variable... to use
+								// it later for the rest request
+								Toast.makeText(getActivity(),
+										person.toString(), Toast.LENGTH_LONG)
+										.show();
+								selectedPerson = person;
+							}
 
-					public void onNothingSelected(AdapterView<?> arg0) {
-						// no operation
+							public void onNothingSelected(AdapterView<?> arg0) {
+								// no operation
+							}
+						});
+
+						ArrayAdapter<Persons> dataAdapterShops = new ArrayAdapter<Persons>(
+								getActivity(),
+								android.R.layout.simple_spinner_item, result);
+						dataAdapterShops
+								.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+						spinner.setAdapter(dataAdapterShops);
 					}
 				});
-
-				ArrayAdapter<Persons> dataAdapterShops = new ArrayAdapter<Persons>(
-						getActivity(),
-						android.R.layout.simple_spinner_item, result);
-				dataAdapterShops
-						.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-				spinner.setAdapter(dataAdapterShops);
-			}
-		});
 		task2.execute(new String[] { getString(R.string.host_url) });
 
+		final Button buttonaddexpense = (Button) theLayout.findViewById(R.id.buttonaddexpense);
+		buttonaddexpense.setOnClickListener(new View.OnClickListener() {
+       	 public void onClick(View v) {
+//       		 HttpPostAsyncTask atask = new HttpPostAsyncTask();
+//       		 atask.execute(new String[] { getString(R.string.host_url) });
+       		 
+//                // Check Login
+//                String username = eingabefeld.getText().toString();
+//                String password = etPassword.getText().toString();
+//                 
+//                if(username.equals("guest") && password.equals("guest")){
+//                    lblResult.setText("Login successful.");
+//                } else {
+//                    lblResult.setText("Login failed. Username and/or password doesn't match.");
+//                }
+//       		 System.out.println(username);
+//       		 textausgabe.setText(username);
+       		Toast.makeText(getActivity(), "Eingabe:" +selectedShop.toString() +" (" +selectedShop.getShopid() +") - " +selectedPerson.toString() +" ("+selectedPerson.getPERSONID() +")", Toast.LENGTH_LONG).show();
+       	 }
+        });
+		
 		return theLayout;
 	}
 
-	class MyData {
-		public MyData(String spinnerText, String value) {
-			this.spinnerText = spinnerText;
-			this.value = value;
-		}
-
-		public String getSpinnerText() {
-			return spinnerText;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		public String toString() {
-			return spinnerText;
-		}
-
-		String spinnerText;
-		String value;
-	}
 }
--- ExpenseTracker/src/pascal/expensetracker/objects/Expense.java
+++ ExpenseTracker/src/pascal/expensetracker/objects/Expense.java
@@ -5,6 +5,24 @@ public class Expense {
 	
 	private Persons person;
 	private JoinedShops shop;
+	private String expensedate;
+	private String expense;
+	
+	public String getExpensedate() {
+		return expensedate;
+	}
+
+	public void setExpensedate(String expensedate) {
+		this.expensedate = expensedate;
+	}
+
+	public String getExpense() {
+		return expense;
+	}
+
+	public void setExpense(String expense) {
+		this.expense = expense;
+	}
 	
 	public Persons getPerson() {
 		return person;
@@ -22,10 +40,12 @@ public class Expense {
 		this.shop = shop;
 	}
 
-	public Expense(Persons person, JoinedShops shop) {
+	public Expense(Persons person, JoinedShops shop, String expensedate, String expense) {
 		super();
 		this.person = person;
 		this.shop = shop;
+		this.expense = expense;
+		this.expensedate = expensedate;
 	}
 
 }
